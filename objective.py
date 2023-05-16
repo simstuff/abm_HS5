7a2c092f83746b73c34b95797edb6b7cfd285274
Niklas Oetken <niklas.oetken@stud.uni-bamberg.de>
2023-05-16 11:26:44 +0200

    add optuna objective

---
+++ objective.py
@@ -0,0 +1,14 @@
+import mesa
+import agent
+import model
+import optuna
+
+def objective(trial):
+
+    params = {
+        "max_steps":
+        "n_agents":,
+        "distribution":,
+        "connections":,
+    }
+    pass
